003139d951175fd0ffed9f584f199e81365c3cb1
Ilia Prokhorov <demensdeum@gmail.com>
2026-01-31 22:41:39 +0400

    fixing rotation

--- client/web/src/entitiesController.js
+++ client/web/src/entitiesController.js
@@ -134,7 +134,7 @@ export class EntitiesController {
                 const targetRotationY = Math.atan2(
                     playerPos.z - enemyPos.z,
                     playerPos.x - enemyPos.x
-                );
+                ) + Math.PI / 2;
 
                 const currentRotation = sceneObject.threeObject.rotation;
                 const currentY = currentRotation.y;
@@ -150,7 +150,7 @@ export class EntitiesController {
                     sceneController.rotateObjectTo(
                         uuid,
                         currentRotation.x,
-                        currentY + diff * smoothing + Math.PI / 2,
+                        currentY + diff * smoothing,
                         currentRotation.z
                     );
                 }
